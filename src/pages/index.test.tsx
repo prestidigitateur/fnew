ea248d05e474a013b434d36066098f3ec48132b7
Tomasz <tomasz@imac-pro-tomas.home>
2022-08-11 17:30:27 +0200

    test updated

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,13 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { beforeEach, describe, expect, test } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { beforeAll, describe, expect, test } from "vitest";
 import Home from ".";
 
 describe("Home", () => {
-  beforeEach(() => {
+  beforeAll(() => {
     render(<Home />);
   });
   test("renders hello world", () => {
     const title = screen.getByText("Hello World!");
     expect(title).toBeDefined();
   });
+  test("reveal hidden content", () => {
+    const btn = screen.getByTestId("show-btn");
+    fireEvent.click(btn);
+    const label = screen.getByText("hidden content");
+    expect(label).toBeDefined();
+  });
+  // test("hide content", () => {
+  //   const btn = screen.getByTestId("show-btn");
+  //   fireEvent.doubleClick(btn);
+  //   // fireEvent.click(btn);
+  //   const label = screen.getByText("hidden content");
+  //   expect(label).toBeUndefined();
+  // });
 });
